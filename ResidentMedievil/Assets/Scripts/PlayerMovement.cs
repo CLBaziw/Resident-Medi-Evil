cd26d75f93e52a2fedd1eaac880f7b5c0870b370
Christina <cbaziw@outlook.com>
2019-11-19 16:07:29 -0500

    Started on movement
    Character has been placed in scene 1. Character can move but will go through walls and camera does not move with character.

---
+++ ResidentMedievil/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Public variables
+    public float moveSpeed = 3f;
+    public Vector2 movement;
+    public Animator animator;
+
+    //Private variables
+    private Rigidbody2D rBody;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();    
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        rBody.MovePosition(rBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
