fb5ba3bf3db0515ab0a7a76a1a4887fae84de709
Christina <cbaziw@outlook.com>
2019-11-20 00:12:56 -0500

    All teleporters added
    To make this easier I decided to leave out the ability to use the teleporter to go back to where you came from. We can put this in later

---
+++ ResidentMedievil/Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        Debug.Log("Trigger");
+
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
