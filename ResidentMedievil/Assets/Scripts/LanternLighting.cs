8a6b5bb31d2099487bb22b9775448571fa75288f
Siyue-comp <55209652+Siyue-comp@users.noreply.github.com>
2019-12-04 16:01:19 -0500

    Added Fog of War

---
+++ ResidentMedievil/Assets/Scripts/LanternLighting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LanternLighting : MonoBehaviour
+{
+    public ChestsPowerUps ChestsPowerUps;
+
+    public Material[] material;
+
+    Renderer rend;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ChestsPowerUps.lantern == true)
+        {
+            rend.sharedMaterial = material[1];
+        }
+    }
+}
